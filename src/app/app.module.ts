1b35f9e56546788fb44d3641fa2603738f85d34e
Ngwako L.E. Mahasha <ngwako.le.mahasha@gmail.com>
2018-10-17 21:48:44 +0200

    starting the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhonebookComponent } from './phonebook/phonebook.component';
+import { AddContactsComponent } from './phonebook/add-contacts/add-contacts.component';
+import { ViewContactsComponent } from './phonebook/view-contacts/view-contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhonebookComponent,
+    AddContactsComponent,
+    ViewContactsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/phonebook/add-contacts/add-contacts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contacts',
+  templateUrl: './add-contacts.component.html',
+  styleUrls: ['./add-contacts.component.scss']
+})
+export class AddContactsComponent implements OnInit {
+
+  constructor() { }
+
+  addToPhonebook() {
+  }
+
+
+  ngOnInit() {
+  }
+
+}
